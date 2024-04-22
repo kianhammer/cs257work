75635b47f78dba95c5f7164f8ffc23688c801b22
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-04-22 10:24:24 -0500

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -26,7 +26,8 @@ def answer_query():
     password="spring997green")
   
   cur = conn.cursor()
-  
+
+  #search for Northfield
   sql_check_Northfield = """SELECT CASE WHEN EXISTS (
       SELECT * FROM citypop WHERE city = 'Northfield'
     )
@@ -38,9 +39,45 @@ def answer_query():
   else:
     print('Northfield does not exist in the database')
 
-  sql_state_pop = """"""
-    
+  
+  #city with largest population
+  sql_city_largest_pop = """SELECT * FROM citypop ORDER BY population DESC LIMIT 1;"""
 
+  print('City with largest population:')
+  cur.execute(sql_city_largest_pop)
+  
+
+  #smallest city in Minnesota
+  sql_city_smallest_minnesota = """SELECT * FROM citypop WHERE state = 'Minnesota' ORDER BY population LIMIT 1;"""
+
+  print('Smallest city in Minnesota:')
+  cur.execute(sql_city_smallest_minnesota)
+
+
+  #cities furthest North/South/East/West
+  sql_city_furthest_north = """SELECT * FROM citypop ORDER BY latitude DESC LIMIT 1;""";
+  sql_city_furthest_south = """SELECT * FROM citypop ORDER BY latitude LIMIT 1;""";
+  sql_city_furthest_west = """SELECT * FROM citypop ORDER BY longitude LIMIT 1;""";
+  sql_city_furthest_east = """SELECT * FROM citypop ORDER BY longitude DESC LIMIT 1;""";
+
+  print('Most northern city:')
+  cur.execute(sql_city_furthest_north)
+  print('Most southern city:')
+  cur.execute(sql_city_furthest_south)
+  print('Most western city:')
+  cur.execute(sql_city_furthest_west)
+  print('Most eastern city:')
+  cur.execute(sql_city_furthest_east)
+
+
+  #user input:
+  stateName = input("Enter state:")
+  print(stateName)
+    
+  
+
+
+  
   conn.commit()
 
 answer_query()
