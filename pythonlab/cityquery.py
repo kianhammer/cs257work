cb12c6455e4b67c121a4fdd3e86d9babc87952da
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-04-22 10:49:29 -0500

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -35,7 +35,8 @@ def answer_query():
     ELSE CAST(0 AS BIT) END;"""
   sql_Northfield = """SELECT * FROM citypop WHERE city = 'Seattle';"""
 
-  return cur.execute(sql_Northfield)
+  cur.execute(sql_Northfield)
+  return cur.fetchone()
   if cur.execute(sql_check_Northfield) == 1:
     return cur.execute(sql_Northfield)
   else:
