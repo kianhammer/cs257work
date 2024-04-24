c31654a30b49de12447397edfc163d338b21c7ae
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-04-24 10:56:52 -0500

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -19,7 +19,7 @@ def my_color(word1):
 @app.route('/add/<num1>/<num2>')
 def my_add(num1, num2):
     number = int(num1) + int(num2)
-    return '' + number
+    return '' + str(number)
 
 if __name__ == '__main__':
     my_port = 5114
