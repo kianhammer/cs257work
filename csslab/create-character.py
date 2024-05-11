b079dc8ab1e34c5ebf79179a5694cf5f0b7f751e
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-10 23:00:15 -0500

    Create create-character.py

---
+++ csslab/create-character.py
@@ -0,0 +1,18 @@
+import flask
+import psycopg2
+from flask import render_template
+import random
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def welcome():
+
+
+  return render_template("homepage.html")
+
+
+
+if __name__ == '__main__':
+    my_port = 5114
+    app.run(host='0.0.0.0', port = my_port) 
